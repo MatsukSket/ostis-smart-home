b9dd3431ce1d27f02a0e20a2af50b40c7afdf8a0
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-11-04 16:32:22 +0300

    scripts: update sc-machine and sc-web

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -15,13 +15,13 @@ export PLATFORM_PATH="${APP_ROOT_PATH}/ostis-web-platform"
 
 export SC_MACHINE_REPO="https://github.com/ostis-ai/sc-machine.git"
 export SC_MACHINE_BRANCH="main"
-export SC_MACHINE_COMMIT="ba8f336524dd908d0d4295d5f04fbdac79b16267"
+export SC_MACHINE_COMMIT="5ec0a1e14304eac345891d0b247ee335140cc8d9"
 export SC_MACHINE_PATH="${PLATFORM_PATH}/sc-machine"
 export ROOT_CMAKE_PATH="${APP_ROOT_PATH}"
 
-export SC_WEB_REPO="https://github.com/NikitaZotov/sc-web.git"
-export SC_WEB_BRANCH="feat/harmonise_sc_types"
-export SC_WEB_COMMIT="fa180ea361f9d1f668170b658cd4c3ea0bcfe82d"
+export SC_WEB_REPO="https://github.com/ostis-ai/sc-web.git"
+export SC_WEB_BRANCH="main"
+export SC_WEB_COMMIT="527a7a728e7d547f480dd106763c1d66a625fad6"
 export SC_WEB_PATH="${PLATFORM_PATH}/sc-web"
 
 if [ -d "${PLATFORM_PATH}" ];
