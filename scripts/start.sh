20139ca1a45fb83cf694ec8a93e17c4327177435
Daniil Semenov <dnk.semenov@gmail.com>
2025-05-23 00:47:42 +0300

    [scripts] update start.sh to improve path handling and usage instructions

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT_PATH="$(cd "$SCRIPT_PATH/.." && pwd)"
+
+PS_COMMON_LIB_PATH="$PROJECT_ROOT_PATH"/install/ps-common-lib/
+SCL_MACHINE_PATH="$PROJECT_ROOT_PATH"/install/scl-machine
+SC_MACHINE_PATH="$PROJECT_ROOT_PATH"/install/sc-machine
+NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH="$PROJECT_ROOT_PATH"/install/non-atomic-action-interpreter-module
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH/lib:$PS_COMMON_LIB_PATH/lib:$SCL_MACHINE_PATH/lib:$SC_MACHINE_PATH/lib:$NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH/lib/extensions:$LD_LIBRARY_PATH"
+
 case "$1" in
-  kb)
-    ./install/sc-machine/bin/sc-builder -i repo.path -o kb.bin --clear
+  build_kb)
+    "$PROJECT_ROOT_PATH"/install/sc-machine/bin/sc-builder -i repo.path -o kb.bin --clear
     ;;
-  server)
-    export LD_LIBRARY_PATH=$PWD/install/ps-common-lib/lib:$PWD/install/sc-machine/lib:$PWD/install/scl-machine/lib:$PWD/install/non-atomic-action-interpreter-module/lib/extensions:$LD_LIBRARY_PATH
-    ./install/sc-machine/bin/sc-machine -s kb.bin \
-      -e "install/sc-machine/lib/extensions;install/scl-machine/lib/extensions;build/Release/extensions;install/non-atomic-action-interpreter-module/lib/extensions" -c ostis-example-app.ini
+  machine)
+    LD_LIBRARY_PATH="$LD_LIBRARY_PATH" \
+      $SC_MACHINE_PATH/bin/sc-machine -s kb.bin \
+      -e "$SC_MACHINE_PATH/lib/extensions;$SCL_MACHINE_PATH/lib/extensions;$NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH/lib/extensions;$PROJECT_ROOT_PATH/build/Release/extensions;" -c ostis-example-app.ini
     ;;
   web)
-    cd interface/sc-web || exit 1
+    cd $PROJECT_ROOT_PATH/interface/sc-web || exit 1
     source .venv/bin/activate
     python3 server/app.py
     ;;
   *)
-    echo "Usage: $0 {kb|server|web}"
+    echo "Usage: $0 {build_kb|machine|web}"  
     exit 1
     ;;
 esac
