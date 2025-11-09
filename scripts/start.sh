0ecd30d01fbf2b0f3e9f835f87828079be91488c
NikitaZotov <nikita.zotov.belarus@gmail.com>
2025-11-09 21:34:09 +0300

    [fix] Dynamic loading of scl-machine and interpreter module

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,8 @@ PS_COMMON_LIB_PATH="$PROJECT_ROOT_PATH"/install/ps-common-lib/
 SCL_MACHINE_PATH="$PROJECT_ROOT_PATH"/install/scl-machine
 SC_MACHINE_PATH="$PROJECT_ROOT_PATH"/install/sc-machine
 NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH="$PROJECT_ROOT_PATH"/install/non-atomic-action-interpreter-module
+FIXED_SEARCH_STRATEGY_TEMPLATE_PROCESSING_MODULE="$PROJECT_ROOT_PATH"/install/fixed-search-strategy-template-processing-module
+
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH/lib:$PS_COMMON_LIB_PATH/lib:$SCL_MACHINE_PATH/lib:$SC_MACHINE_PATH/lib:$NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH/lib/extensions:$LD_LIBRARY_PATH"
 
 case "$1" in
@@ -17,7 +19,7 @@ case "$1" in
   machine)
     LD_LIBRARY_PATH="$LD_LIBRARY_PATH" \
       $SC_MACHINE_PATH/bin/sc-machine -s kb.bin \
-      -e "$SC_MACHINE_PATH/lib/extensions;$SCL_MACHINE_PATH/lib/extensions;$NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH/lib/extensions;$PROJECT_ROOT_PATH/build/Release/extensions;" -c ostis-example-app.ini
+      -e "$SC_MACHINE_PATH/lib/extensions;$SCL_MACHINE_PATH/lib/extensions;$NON_ATOMIC_ACTION_INTERPRETER_MODULE_PATH/lib/extensions;$FIXED_SEARCH_STRATEGY_TEMPLATE_PROCESSING_MODULE/lib/extensions;$PROJECT_ROOT_PATH/build/Release/extensions" -c ostis-example-app.ini
     ;;
   web)
     cd $PROJECT_ROOT_PATH/interface/sc-web || exit 1
